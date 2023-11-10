869c5cffe080e5cfa82b4024192ab3409b3a82b3
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-11-10 19:56:45 +0800

    Remove user creation from database initialisation

--- src/jobs/database_initialiser/config.ts
+++ src/jobs/database_initialiser/config.ts
@@ -7,11 +7,6 @@ export default class Config {
   /** Variable names that are found in environment.  */
 
   private static readonly _envMongoURI: string = 'MS_MONGO_URI';
-
-  private static readonly _envMongoUser: string = 'MS_MONGO_USER';
-  private static readonly _envMongoPass: string = 'MS_MONGO_PASS';
-
-  private static readonly _envMongoDB: string = 'MS_MONGO_DB';
   private static readonly _envMongoCollection: string = 'MS_MONGO_COLLECTION';
 
   /** Copies from Environment and save into these variable names. */
@@ -20,19 +15,6 @@ export default class Config {
    * Connection to the mongo database.
    */
   public readonly mongoURI: string;
-
-  /**
-   * Username required to create the database user.
-   */
-  public readonly mongoUser: string;
-  /**
-   * Password required to create the database user.
-   */
-  public readonly mongoPass: string;
-  /**
-   * The database to be created.
-   */
-  public readonly mongoDB: string;
   /**
    * The collection to be created.
    */
@@ -50,20 +32,14 @@ export default class Config {
     debug: boolean = false,
   ) {
     if (debug) {
-      this.mongoUser = 'user';
-      this.mongoPass = 'password';
-      this.mongoDB = 'matchinginfo';
-      this.mongoCollection = 'queueinfo';
       this.mongoURI = `mongodb://admin:password@localhost:27017`;
+      this.mongoCollection = 'queueinfo';
     } else {
-      this.mongoURI = this._getEnvAsString(env, Config._envMongoURI);
-      this.mongoUser = this._getEnvAsString(env, Config._envMongoUser);
-      this.mongoPass = this._getEnvAsString(env, Config._envMongoPass);
-      this.mongoDB = this._getEnvAsString(env, Config._envMongoDB);
       this.mongoCollection = this._getEnvAsString(
         env,
         Config._envMongoCollection,
       );
+      this.mongoURI = this._getEnvAsString(env, Config._envMongoURI);
     }
   }
 
@@ -85,23 +61,6 @@ export default class Config {
   }
 
   /**
-   * Retrieves the int value of key from Environments.
-   * @param env - NodeJS.ProcessEnv.
-   * @param key - The environment variable name.
-   * @returns The int value of the variable.
-   * @throws Error if unable to process the key.
-   */
-  private _getEnvAsInt(env: NodeJS.ProcessEnv, key: string): number {
-    if (env[key] !== undefined) {
-      const ret = this._parseInt(env[key]);
-      if (ret !== undefined) {
-        return ret;
-      }
-    }
-    throw Error(`${key} is not set in env or is not a string.`);
-  }
-
-  /**
    * Returns undefined if string is empty or undefined.
    * @param raw - The string to be parsed.
    * @returns The string or undefined.
@@ -111,23 +70,5 @@ export default class Config {
       return undefined;
     }
     return raw;
-  }
-
-  /**
-   * Returns undefined if Integer is not a number or undefined.
-   * @param raw - The string to be parsed.
-   * @returns The string or undefined.
-   */
-  private _parseInt(raw: string | undefined): number | undefined {
-    if (raw === undefined) {
-      return undefined;
-    }
-
-    const val: number = parseFloat(raw);
-    if (isNaN(val) || !Number.isInteger(val)) {
-      return undefined;
-    }
-
-    return val;
   }
 }
