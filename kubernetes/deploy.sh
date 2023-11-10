507560bad4e8d989909a83abeb42631e479903f8
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-11-10 17:59:54 +0800

    Add kubernetes config files

---
+++ kubernetes/deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+kubectl apply -f ./config_maps/core.yaml
+kubectl apply -f ./config_maps/database_client.yaml
+
+kubectl apply -f ./config_maps/database_initialiser.yaml
+kubectl apply -f ./secrets/database_initialiser.yaml
+kubectl apply -f ./jobs/database_initialiser.yaml
+
+kubectl apply -f ./secrets/api.yaml
+kubectl apply -f ./deployments/api.yaml
+kubectl apply -f ./services/api.yaml
+kubectl apply -f ./hpas/api.yaml
